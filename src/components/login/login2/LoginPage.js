05cb7dbd2aa662e1a9dfcb0999799ab31f336ba4
sampath surineni <ssurineni@salesforce.com>
2024-01-19 17:58:13 +0530

    made component height 100%

--- src/components/login/login2/LoginPage.js
+++ src/components/login/login2/LoginPage.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './LoginPage2.css';
 function LoginPage() {
     return (
-        <div className='login-box2'>
-            <div className='form-box'>
-                <form>
-                    <input type="text" placeholder='Username' />
-                    <input type="password" placeholder='Password' />
-                    <button>Login</button>
-                </form>
+        <div className='component-box'>
+            <div className='login-box2'>
+                <div className='form-box'>
+                    <form>
+                        <input type="text" placeholder='Username' />
+                        <input type="password" placeholder='Password' />
+                        <button>Login</button>
+                    </form>
+                </div>
             </div>
         </div>
     );
